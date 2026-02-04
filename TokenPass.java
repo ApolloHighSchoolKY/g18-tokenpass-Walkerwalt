e91830644acd59063b72c7a33a3634f54b718182
Walkerwalt <walker.burden@stu.daviess.kyschools.us>
2026-02-04 17:54:02 +0000

    unfinished

--- TokenPass.java
+++ TokenPass.java
@@ -1,9 +1,28 @@
 public TokenPass(int playerCount){
   /*  to be answered in part (a) */
-  
+  board[] = new int[playerCount]
+
+  for(int x = 0; x<board.length; x++)
+  {
+    int randNum = (int)(Math.Random() * 10) + 1;
+
+    board[x] = randNum;
+    currentPlayer = (int)(Math.Random() * playerCount);
+  }
 }
 
 public void distributeCurrentPlayerTokens(){
   /* to be answered in part (b) */
-  
+  int toPlayer = board[currentPlayer + 1]
+
+  while (board[currentPlayer] > 1)
+  {
+    if (toPlayer > board.length - 1)
+    {
+      toPlayer = 0
+    }
+    else if ()
+    board[toPlayer] += 1
+    board[currentPlayer] -= 1
+  }
 }
